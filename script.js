8784a8314aefb2e3070cb0d6bd2cb5b71dd653e6
kacey_c <kaceycreative@gmail.com>
2021-08-10 13:45:14 -0400

    Mobile styles

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const buttonFtThree = document.querySelector(".feature-3");
 const ftDescripOne = document.querySelector(".feature-1-description");
 const ftDescripTwo= document.querySelector(".feature-2-description");
 const ftDescripThree = document.querySelector(".feature-3-description");
+const ftDescripOneMobile = document.querySelector(".feature-1-mobile");
+const ftDescripTwoMobile = document.querySelector(".feature-2-mobile");
+const ftDescripThreeMobile = document.querySelector(".feature-3-mobile");
 
 // Click Fit Guide
 buttonFtOne.addEventListener("click", function(e){
@@ -13,6 +16,9 @@ buttonFtOne.addEventListener("click", function(e){
     ftDescripOne.classList.remove("hide");
     ftDescripTwo.classList.add("hide");
     ftDescripThree.classList.add("hide");
+    ftDescripOneMobile.classList.remove("hide");
+    ftDescripTwoMobile.classList.add("hide");
+    ftDescripThreeMobile.classList.add("hide");
 });
 
 // Click Care
@@ -23,6 +29,9 @@ buttonFtTwo.addEventListener("click", function(e){
     ftDescripOne.classList.add("hide");
     ftDescripTwo.classList.remove("hide");
     ftDescripThree.classList.add("hide");
+    ftDescripOneMobile.classList.add("hide");
+    ftDescripTwoMobile.classList.remove("hide");
+    ftDescripThreeMobile.classList.add("hide");
 });
 
 // Click Materials
@@ -33,4 +42,7 @@ buttonFtThree.addEventListener("click", function(e){
     ftDescripOne.classList.add("hide");
     ftDescripTwo.classList.add("hide");
     ftDescripThree.classList.remove("hide");
+    ftDescripOneMobile.classList.add("hide");
+    ftDescripTwoMobile.classList.add("hide");
+    ftDescripThreeMobile.classList.remove("hide");
 });
